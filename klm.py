bdedb7d218f9eb4e7ece2e4f6712e281b6532356
KayBrinkmann <57799266+KayBrinkmann@users.noreply.github.com>
2021-05-22 16:40:48 +0100

    Add files via upload

--- klm.py
+++ klm.py
@@ -1,14 +1,6 @@
 # This Python file uses the following encoding: utf-8
-
-
-klm_own_model = {}
-klm_standard_model = {
-    "k": 0.28,
-    "p": 1.1,
-    "h": 0.4,
-    "b": 0.1,
-    "m": 1.35
-}
+import sys
+import json
 
 
 def file_to_klm_string(filename):
@@ -19,8 +11,7 @@ def file_to_klm_string(filename):
         return []
     lines = file.readlines()
     for line in lines:
-        split = line.split("#",1)
-        print("line : ", split)
+        split = line.split("#", 1)
         new_line = split[0]
         klm_string += new_line
 
@@ -43,11 +34,33 @@ def calculate_klm_time(klm_model, klm_string):
             if len(numbers) > 0:
                 operator_number *= float(numbers)
                 numbers = ""
-            print("+", operator_number)
             result += operator_number
     return result
 
 
-klm_string = file_to_klm_string("klm.txt")
-print("klm string: ", file_to_klm_string("klm.txt"))
-print("result with standard klm values: ", calculate_klm_time(klm_standard_model, klm_string))
+def parse_config(file):
+    try:
+        with open(str(file)) as f:
+            setup_dict = json.load(f)
+    except Exception as e:
+        print(e, ": could not read setup file!")
+        sys.exit()
+    return setup_dict
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.stderr.write("you need to pass a setup file!")
+        sys.exit()
+    klm_dict = parse_config(sys.argv[1])
+    try:
+        klm_string = file_to_klm_string(klm_dict["operator_file"])
+    except Exception as e:
+        print(e, ": could not read operator file!")
+        sys.exit()
+    print("klm string: ", file_to_klm_string("klm.txt"))
+    print("Expected task completion time = ", calculate_klm_time(klm_dict, klm_string))
+
+
+if __name__ == '__main__':
+    main()
